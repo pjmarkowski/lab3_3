41b145da2c82c2a556f107467aa2cb4cf24f0b83
195024 <195024@edu.p.lodz.pl>
2017-03-22 09:05:49 +0100

    Created constant ONE_HOUR, test for confirm() throwing exception is positive

--- src/main/java/fakeSystemClock/AdvancedTimeSrc.java
+++ src/main/java/fakeSystemClock/AdvancedTimeSrc.java
@@ -3,10 +3,11 @@ package fakeSystemClock;
 public final class AdvancedTimeSrc implements TimeSource {
 
 	private static final long ONE_DAY = 24 * 60 * 60 * 1000;
-
+	private static final long ONE_HOUR = 60 * 60 * 1000;
+	
 	@Override
 	public long currentTimeMillis() {
-		return System.currentTimeMillis() + ONE_DAY;
+		return System.currentTimeMillis() + ONE_DAY + ONE_HOUR;
 	}
 
 }
