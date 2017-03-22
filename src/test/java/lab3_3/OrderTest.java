1ee880f4bc8e341d92a045f1fecc9fc23dc39bf5
mateusz.sadowski <195054@edu.p.lodz.pl>
2017-03-22 13:51:05 +0100

    Order test case added - checking if 25h is already expired

--- src/test/java/lab3_3/OrderTest.java
+++ src/test/java/lab3_3/OrderTest.java
@@ -4,12 +4,21 @@ import org.junit.Test;
 
 import edu.iis.mto.time.AlteredTimeProvider;
 import edu.iis.mto.time.Order;
+import edu.iis.mto.time.OrderExpiredException;
 
 public class OrderTest {
 
 	@Test
 	public void orderNotYetExpired() {
 		AlteredTimeProvider.howMuchToAdd = 24 * 60 * 60 * 1000;
+		Order testOrder = new Order();
+		testOrder.submit();
+		testOrder.confirm();
+	}
+	
+	@Test (expected = OrderExpiredException.class)
+	public void orderExpired() {
+		AlteredTimeProvider.howMuchToAdd = 24 * 60 * 60 * 1000 + 60 * 60 * 1000;
 		Order testOrder = new Order();
 		testOrder.submit();
 		testOrder.confirm();
