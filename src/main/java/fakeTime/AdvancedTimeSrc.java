54645cbc5eb729ca9afd08990cd48bf42a75e832
Konrad Sochacki <sochacki.konrad@gmail.com>
2017-03-23 09:58:42 +0100

    ONE_HOUR in AdvancedTimeSrc added

--- src/main/java/fakeTime/AdvancedTimeSrc.java
+++ src/main/java/fakeTime/AdvancedTimeSrc.java
@@ -1,11 +1,12 @@
 package fakeTime;
 
 public final class AdvancedTimeSrc implements TimeSource {	 
-	 	private static final long ONE_DAY = 24 * 60 * 60 * 1000;	 	
+	 	private static final long ONE_DAY = 24 * 60 * 60 * 1000;
+	 	private static final long ONE_HOUR = 60 * 60 * 1000;	
 	 
 	 	@Override
 	 	public long currentTimeMillis() {
-	 		return System.currentTimeMillis() + ONE_DAY;
+	 		return System.currentTimeMillis() + ONE_DAY + ONE_HOUR;
 	 	}
 	
 	 }
