7eab2e846a5c605068dbcc06c3dd8f92da949b32
mateusz.sadowski <195054@edu.p.lodz.pl>
2017-03-26 13:04:54 +0200

    Changes requested - rewrote tests to use new Order constructor

--- src/test/java/lab3_3/OrderTest.java
+++ src/test/java/lab3_3/OrderTest.java
@@ -1,31 +1,25 @@
 package lab3_3;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import edu.iis.mto.time.AlteredTimeProvider;
 import edu.iis.mto.time.Order;
 import edu.iis.mto.time.OrderExpiredException;
+import edu.iis.mto.time.TimeProvider;
 
 public class OrderTest {
 	
-	public Order testOrder;
-	
-	@Before
-	public void setupTests() {
-		testOrder = new Order();
-		testOrder.submit();
-	}
-	
 	@Test
 	public void orderNotYetExpired() {
-		AlteredTimeProvider.howMuchToAdd = 24 * 60 * 60 * 1000;
+		Order testOrder = new Order(new TimeProvider());
+		testOrder.submit();
 		testOrder.confirm();
 	}
 	
 	@Test (expected = OrderExpiredException.class)
 	public void orderExpired() {
-		AlteredTimeProvider.howMuchToAdd = 24 * 60 * 60 * 1000 + 60 * 60 * 1000;
+		Order testOrder = new Order(new AlteredTimeProvider());
+		testOrder.submit();
 		testOrder.confirm();
 	}
 }
