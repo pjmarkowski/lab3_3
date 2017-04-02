9a7b252abeaf23cd9b54e9cc619876f0943d2b55
Andrzej Borzecki <andrew.borzecki@gmail.com>
2017-04-02 02:20:50 +0200

    Added test case for not expired order (SystemTimeProvider used)

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,14 +1,17 @@
 package edu.iis.mto.time;
 
-import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Created by grusz on 02.04.2017.
  */
 public class OrderTest {
-    @Before
-    public void setUp() throws Exception {
-        
+
+    @Test
+    public void checkIfOrderExpired_notExpiredOrder() throws Exception {
+        Order order = new Order(new SystemTimeProvider());
+        order.submit();
+        order.confirm();
 
     }
 }
