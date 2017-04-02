441650cf107cbc4abfd933fd409738100a2649bf
Andrzej Borzecki <andrew.borzecki@gmail.com>
2017-04-02 02:15:36 +0200

    Created Order test class

---
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -0,0 +1,14 @@
+package edu.iis.mto.time;
+
+import org.junit.Before;
+
+/**
+ * Created by grusz on 02.04.2017.
+ */
+public class OrderTest {
+    @Before
+    public void setUp() throws Exception {
+        
+
+    }
+}
