016d6d13093af2a766185b69484dc88dfee96205
Andrzej Borzecki <andrew.borzecki@gmail.com>
2017-04-02 02:32:56 +0200

    Added test case for expired order (CustomTimeProvider used)

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,6 +1,9 @@
 package edu.iis.mto.time;
 
+
 import org.junit.Test;
+
+
 
 /**
  * Created by grusz on 02.04.2017.
@@ -14,4 +17,13 @@ public class OrderTest {
         order.confirm();
 
     }
+
+    @Test (expected = OrderExpiredException.class)
+    public void checkIfOrderExpired_ExpiredOrder() throws Exception {
+        Order order = new Order(new CustomTimeProvider());
+        order.submit();
+        order.confirm();
+    }
+
+
 }
