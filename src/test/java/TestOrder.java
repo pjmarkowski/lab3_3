90fa43577ac21a80f183f1f291f00542935fe1d1
Piotr Markowski <piotrjakubmarkowski@gmail.com>
2017-04-24 18:24:26 +0200

    Stworzenie klasy testowej

---
+++ src/test/java/TestOrder.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Piotr on 24.04.2017.
+ */
+public class TestOrder {
+
+    @Test
+    public void test() {
+        fail("fail");
+    }
+}
